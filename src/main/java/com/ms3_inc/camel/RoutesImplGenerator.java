5c3e2a971c2bc53a5e0eca587871d73b6899985f
R. L. Ratcliffe <rlr@tcliffe.dev>
2021-01-08 15:04:58 -0500

    Fix bug in implementation class

--- src/main/java/com/ms3_inc/camel/RoutesImplGenerator.java
+++ src/main/java/com/ms3_inc/camel/RoutesImplGenerator.java
@@ -12,7 +12,7 @@ public class RoutesImplGenerator extends RoutesGenerator {
         generatedCode.append(tabs(NO_INDENT)).append("from(direct(\"").append(opId).append("\"))\n");
         generatedCode.append(tabs(ONE_TAB_INDENT)).append(".setBody(DatasonnetExpression.builder(\"{opId: '");
         generatedCode.append(opId);
-        generatedCode.append("'}, String.class)\n").append(tabs(THREE_TAB_INDENT)).append(".outputMediaType(MediaTypes.APPLICATION_JSON))\n");
+        generatedCode.append("'}\", String.class)\n").append(tabs(THREE_TAB_INDENT)).append(".outputMediaType(MediaTypes.APPLICATION_JSON))\n");
         generatedCode.append(tabs(NO_INDENT)).append(";\n");
 
         return generatedCode;
